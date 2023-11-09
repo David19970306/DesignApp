3e2f4539d4bf58fb479a1b40b757a3d1c43d091f
吴伟鹏 <2662345338@qq.com>
2023-11-09 18:27:11 +0800

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -15,6 +15,18 @@ selected_file = st.sidebar.selectbox("选择要使用的功能", markdown_files)
 
 # 选择文件并重命名
 uploaded_file = st.file_uploader("选择文件", type=["png", "jpg"])
+# 保存文件
+if uploaded_file is not None:
+    temp_file = NamedTemporaryFile(delete=True)
+    buffer = uploaded_file.getbuffer()
+    if buffer:
+        temp_file.write(buffer)
+        image_file = open(temp_file.name, 'rb')
+        image_bytes = image_file.read()
+        st.image(image_bytes)
+    else:
+        st.error("请上传需要设计的草图")
+
 
 # 请选择一个设计风格
 designStyle = st.selectbox(
@@ -33,16 +45,10 @@ else:
 weight = st.slider('ai weight:', 0.0, 0.1, 1.0)
 st.write("AI权重为：", weight)
 
+if st.button('开始生成'):
+    st.write('生成如下')
+else:
+    st.write('Goodbye')
     
 
-# 保存文件
-if uploaded_file is not None:
-    temp_file = NamedTemporaryFile(delete=True)
-    buffer = uploaded_file.getbuffer()
-    if buffer:
-        temp_file.write(buffer)
-        image_file = open(temp_file.name, 'rb')
-        image_bytes = image_file.read()
-        st.image(image_bytes)
-    else:
-        st.error("请上传需要设计的草图")
+
