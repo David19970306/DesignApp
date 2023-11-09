83fc22bd7718fee944ed69fb272a69f03c0d3cf1
吴伟鹏 <2662345338@qq.com>
2023-11-09 18:14:03 +0800

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,6 +1,7 @@
 
 import os
 import streamlit as st
+from PIL import Image
 
 # 创建一个文件夹用于保存上传的文件
 if not os.path.exists("uploads"):
@@ -32,12 +33,12 @@ else:
 weight = st.slider('ai weight:', 0.0, 0.1, 1.0)
 st.write("AI权重为：", weight)
 
+
+
 # 保存文件
 if uploaded_file is not None:
     if file_name.strip():
-        file_path = os.path.join("uploads", file_name+".docx")
-        with open(file_path, "wb") as f:
-            f.write(uploaded_file.getbuffer())
-        st.success(f"已保存文件: {file_path}")
+        image = Image.open(uploaded_file.getbuffer())
+        st.image(image, caption='图片预览', use_column_width=True)
     else:
         st.error("请上传需要设计的草图")
